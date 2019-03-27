e8613be2adfe0032b14557a7b51f709425c3cc1b
Jacob Greenwald <jeg4t@virginia.edu>
2019-03-26 20:46:27 -0400

    linter errors and more test cases

--- tests/testSearch.js
+++ tests/testSearch.js
@@ -4,25 +4,27 @@ const { expect } = require('chai')
 const { describe } = mocha
 const { it } = mocha
 
-const Search =  require('../src/lib/Search')
-const searchClass = new Search
+const Search = require('../src/lib/Search')
+
+const searchClass = new Search()
 
 describe('Search', () => {
-	describe('constructMetaSearchUrl', () => {
-		it('return a string', () => {
-			const searchString = searchClass.constructMetaSearchUrl('creator', 'test')
-			expect(searchString).to.be.a('string')
-		})
-		it('string includes base url and function params', () => {
-			const searchString = searchClass.constructMetaSearchUrl('creator', 'test')
-			expect(searchString).to.satisfy(string =>
-				['test', 'creator', 'https://archive.org/advancedsearch.php'].every(bit => string.includes(bit))
-			)
-		})
-	})
-	describe('searchByArtist', () => {
-		it('return a string', () => {
-			expect(searchClass.searchByArtist('test')).to.be.a('string')
-		})
-	})
-})	
+  describe('constructMetaSearchUrl', () => {
+    it('return a string', () => {
+      const searchString = searchClass.constructMetaSearchUrl('creator', 'test')
+      expect(searchString).to.be.a('string')
+    })
+    it('string includes base url and function params', () => {
+      const searchString = searchClass.constructMetaSearchUrl('creator', 'test')
+      expect(searchString).to.satisfy(string => ['test', 'creator', 'https://archive.org/advancedsearch.php'].every(bit => string.includes(bit)))
+    })
+  })
+  describe('searchByArtist', () => {
+    it('return a string', () => {
+      expect(searchClass.searchByArtist('test')).to.be.a('string')
+    })
+    it('should include the url and url params', () => {
+      expect(searchClass.searchByArtist('test')).to.satisfy(string => ['test', 'creator', 'https://archive.org/advancedsearch.php'].every(bit => string.includes(bit)))
+    })
+  })
+})
