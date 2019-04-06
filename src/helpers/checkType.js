5c6c62f0054cdb96574c4297285e862cfc7435c6
Jacob Greenwald <jeg4t@virginia.edu>
2019-04-05 20:12:37 -0400

    adding functions to check for objects and arrays

--- src/helpers/checkType.js
+++ src/helpers/checkType.js
@@ -1,5 +1,11 @@
-function isString(stringToCheck) {
-  return Object.prototype.toString.call(stringToCheck) === '[object String]'
+module.exports = {
+  isString(stringToCheck) {
+    return Object.prototype.toString.call(stringToCheck) === '[object String]'
+  },
+  isArray(arrayToCheck) {
+    return Object.prototype.toString.call(arrayToCheck) === '[object Array]'
+  },
+  isObject(objectToCheck) {
+    return Object.prototype.toString.call(objectToCheck) === '[object Object]'
+  },
 }
-
-module.exports = isString
