6e1dd4e0ffac8b69341e5a7368ab1df3dafa21df
Jacob Greenwald <jeg4t@virginia.edu>
2019-04-05 20:13:21 -0400

    adding function to generate url from passed in options

--- src/lib/Search.js
+++ src/lib/Search.js
@@ -1,19 +1,48 @@
+/* eslint-disable no-console */
 const superagent = require('superagent')
 // const jsonp = require('superagent-jsonp')
 // const jsonp = require('jsonp')
 const jsonp = require('superagent-jsonp')
 
-const isString = require('../helpers/checkType')
+const checkType = require('../helpers/checkType')
 
 class Search {
   /**
    * Class with methods to search archive.org's api
    * @constructor
    */
-  constructor() {
+  constructor(options) {
+    console.log(options)
     this.metaSearchBaseUrl = 'https://archive.org/advancedsearch.php'
-    this.metaSearchDefaults = '&fl%5B%5D=identifier&fl%5B%5D=mediatype&fl%5B%5D=title&&fl%5B%5D=description&fl%5B%5D=year&sort%5B%5D=year+asc&sort%5B%5D=&sort%5B%5D=&rows=20000&page=&output=json'
+    this.metaSearchDefaults = '&fl%5B%5D=identifier&fl%5B%5D=mediatype&fl%5B%5D=title&fl%5B%5D=description&fl%5B%5D=year&sort%5B%5D=year+asc&sort%5B%5D=&sort%5B%5D=&rows=120&page=&output=json'
+    console.log(decodeURI(this.metaSearchDefaults))
     this.results = []
+  }
+
+  /**
+   * override default search options and build new search url.
+   * @param {array} options - Which fields to return from api.
+   */
+  setOptions(options) {
+    if (!checkType.isObject(options)) {
+      throw new TypeError('setOptions() expected an Array')
+    }
+    const invalidOptionsError = 'Invalid syntax for options. Ex: {fields: [...fields], max: 100(optional)}'
+    Object.keys(options).forEach((option) => {
+      if (option !== 'fields' && option !== 'max') {
+        throw new Error(invalidOptionsError)
+      }
+    })
+    if (!checkType.isArray(options.fields)) {
+      throw new Error(invalidOptionsError)
+    }
+    this.metaSearchDefaults = encodeURI('&fl[]=identifier')
+
+    options.fields.forEach((field) => {
+      this.metaSearchDefaults += encodeURI(`&fl[]=${field}`)
+    })
+    if (options.max) this.metaSearchDefaults += `&rows=${options.max}&page=`
+    this.metaSearchDefaults += '&output=json'
   }
 
   /**
@@ -38,17 +67,13 @@ class Search {
           timeout: 3000,
         })).end((err, response) => {
           // response => {}
+          if (err) reject(err)
           if (!response) {
-            throw new Error('Null Response')
-          }
-          if (err) {
-            reject(err)
+            throw new Error('Null Response', null)
           } else {
             const { body } = response
             const { numFound } = body.response
-            if (numFound === 0) {
-              throw new Error('no results, please update query params')
-            }
+            if (numFound === 0) throw new Error('no results, please update query params')
             const { docs } = body.response
             resolve(docs)
           }
@@ -60,16 +85,17 @@ class Search {
    * search archive.org by artist.
   * @param {string} searchTerm - The search term.
   */
-  searchByArtist(searchTerm) {
+  searchByArtist(searchTerm, options = []) {
     // throw error if string is not passed into function
-    if (!isString(searchTerm)) {
+    if (!checkType.isString(searchTerm)) {
       throw new TypeError('searchByArtist() expected a string')
     }
+    if (options.length !== 0) this.setOptions(options)
     // finsish constructing url
     const searchType = 'creator'
     const constructUrlFromParams = this.constructMetaSearchUrl(searchType, searchTerm)
-    const test = this.constructor.makeSearch(constructUrlFromParams)
-    return test.then(result => result)
+    const search = this.constructor.makeSearch(constructUrlFromParams)
+    return search.then(result => result)
     // return constructUrlFromParams
   }
 }
