350c4c78a3f280bbc1d8b9d4838881182277543d
Jacob Greenwald <jeg4t@virginia.edu>
2019-04-23 21:03:42 -0400

    modify tests now that created class is being exported

--- tests/testSearch.js
+++ tests/testSearch.js
@@ -6,34 +6,33 @@ const { describe } = mocha
 const { it } = mocha
 const sinon = require('sinon')
 
-const Search = require('../src/lib/Search')
-
-const searchClass = new Search()
+const { archiveSearch } = require('../src/lib/Search')
+const { Search } = require('../src/lib/Search')
 
 describe('Search', () => {
   describe('constructMetaSearchUrl', () => {
     it('should be called once', () => {
-      sinon.spy(searchClass, 'constructMetaSearchUrl')
-      searchClass.constructMetaSearchUrl('creator', 'test')
-      expect(searchClass.constructMetaSearchUrl.calledOnce).to.be.true
+      sinon.spy(archiveSearch, 'constructMetaSearchUrl')
+      archiveSearch.constructMetaSearchUrl('creator', 'test')
+      expect(archiveSearch.constructMetaSearchUrl.calledOnce).to.be.true
     })
     it('return a string', () => {
-      const searchString = searchClass.constructMetaSearchUrl('creator', 'test')
+      const searchString = archiveSearch.constructMetaSearchUrl('creator', 'test')
       expect(searchString).to.be.a('string')
     })
     it('string includes base url and function params', () => {
-      const searchString = searchClass.constructMetaSearchUrl('creator', 'test')
+      const searchString = archiveSearch.constructMetaSearchUrl('creator', 'test')
       expect(searchString).to.satisfy(string => ['test', 'creator', 'https://archive.org/advancedsearch.php'].every(bit => string.includes(bit)))
     })
   })
   describe('searchByArtist', () => {
     it('should be called once', () => {
-      sinon.spy(searchClass, 'searchByArtist')
-      searchClass.searchByArtist('test')
-      expect(searchClass.searchByArtist.calledOnce).to.be.true
+      sinon.spy(archiveSearch, 'searchByArtist')
+      archiveSearch.searchByArtist('test')
+      expect(archiveSearch.searchByArtist.calledOnce).to.be.true
     })
     it('return an array of objects', () => {
-      const search = searchClass.searchByArtist('test')
+      const search = archiveSearch.searchByArtist('test')
       search.then((result) => {
         expect(result).to.be.an('array')
         result.every(i => expect(i).to.be.an('object'))
@@ -41,7 +40,7 @@ describe('Search', () => {
     })
   })
   describe('setOptions', () => {
-    sinon.spy(searchClass, 'setOptions')
+    sinon.spy(archiveSearch, 'setOptions')
     beforeEach(() => {
       const options = {
         fields: [
@@ -52,19 +51,19 @@ describe('Search', () => {
         ],
         max: 50,
       }
-      searchClass.setOptions(options)
+      archiveSearch.setOptions(options)
     })
     it('should be called once', () => {
-      expect(searchClass.setOptions.calledOnce).to.be.true
+      expect(archiveSearch.setOptions.calledOnce).to.be.true
     })
     it('should take in an argument representing api search options', () => {
-      expect(searchClass.setOptions.getCall(0).args[0]).to.not.be.false
+      expect(archiveSearch.setOptions.getCall(0).args[0]).to.not.be.false
     })
     it('options argument should be an object', () => {
-      expect(searchClass.setOptions.getCall(0).args[0]).to.be.an('object')
+      expect(archiveSearch.setOptions.getCall(0).args[0]).to.be.an('object')
     })
     it('options.fields should be an array', () => {
-      expect(searchClass.setOptions.getCall(0).args[0].fields).to.be.an('array')
+      expect(archiveSearch.setOptions.getCall(0).args[0].fields).to.be.an('array')
     })
   })
   describe('setSortBy', () => {
