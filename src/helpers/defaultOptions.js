b518c58414830cfff327f3de40b533bc09407d18
Jacob Greenwald <jeg4t@virginia.edu>
2019-04-23 21:02:54 -0400

    moving default options into itsown file

---
+++ src/helpers/defaultOptions.js
@@ -0,0 +1,4 @@
+
+const defaultOptions = '&fl%5B%5D=identifier&fl%5B%5D=mediatype&fl%5B%5D=title&fl%5B%5D=description&fl%5B%5D=year&sort%5B%5D=year+asc&sort%5B%5D=&sort%5B%5D=&rows=120&page=&output=json'
+
+module.exports = defaultOptions
