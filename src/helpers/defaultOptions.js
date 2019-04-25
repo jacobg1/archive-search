03417ee8fbf6f56dd4fa55a466113c89710cc275
Jacob Greenwald <jeg4t@virginia.edu>
2019-04-24 21:12:56 -0400

    refactoring

--- src/helpers/defaultOptions.js
+++ src/helpers/defaultOptions.js
@@ -1,4 +1,6 @@
 
-const defaultOptions = '&fl%5B%5D=identifier&fl%5B%5D=mediatype&fl%5B%5D=title&fl%5B%5D=description&fl%5B%5D=year&sort%5B%5D=year+asc&sort%5B%5D=&sort%5B%5D=&rows=120&page=&output=json'
+const defaultOptions = `&fl%5B%5D=identifier&fl%5B%5D=mediatype&fl%5B%5D=
+      title&fl%5B%5D=description&fl%5B%5D=year&sort%5B%5D=
+      year+asc&sort%5B%5D=&sort%5B%5D=&rows=120&page=&output=json`
 
 module.exports = defaultOptions
