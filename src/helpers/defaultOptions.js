731dfd8148ce90f7eb64053db1e877da198f9349
Jacob Greenwald <jeg4t@virginia.edu>
2019-04-27 17:03:33 -0400

    updating default options

--- src/helpers/defaultOptions.js
+++ src/helpers/defaultOptions.js
@@ -1,6 +1,6 @@
 
-const defaultOptions = `&fl%5B%5D=identifier&fl%5B%5D=mediatype&fl%5B%5D=
-      title&fl%5B%5D=description&fl%5B%5D=year&sort%5B%5D=
-      year+asc&sort%5B%5D=&sort%5B%5D=&rows=120&page=&output=json`
+const defaultOptions = `&fl%5B%5D=year&fl%5B%5D=title&fl%5B%5D=mediatype&fl%5B%5D=
+      language&fl%5B%5D=date&fl%5B%5D=downloads&fl%5B%5D=creator&sort%5B%5D=
+      year+asc&sort%5B%5D=downloads+asc&rows=150&page=&output=json`
 
 module.exports = defaultOptions
