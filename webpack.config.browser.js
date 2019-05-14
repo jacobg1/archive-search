1b848aca97d935c4d552380e3f590f7cf8c75a49
Jacob Greenwald <jeg4t@virginia.edu>
2019-05-13 21:01:49 -0400

    update webpack config

--- webpack.config.browser.js
+++ webpack.config.browser.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, './src/dist'),
 		filename: 'search.js',
 	},
 	devtool: 'inline-source-map',
