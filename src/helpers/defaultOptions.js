7e5531342d9c02ce416073ffef13adce2f94169b
Jacob Greenwald <jeg4t@virginia.edu>
2019-05-19 16:33:47 -0400

    fixing default options formatting

--- src/helpers/defaultOptions.js
+++ src/helpers/defaultOptions.js
@@ -1,6 +1,4 @@
 
-const defaultOptions = `&fl%5B%5D=year&fl%5B%5D=title&fl%5B%5D=mediatype&fl%5B%5D=
-      language&fl%5B%5D=date&fl%5B%5D=downloads&fl%5B%5D=creator&&fl%5B%5D=identifier&sort%5B%5D=
-      year+asc&sort%5B%5D=downloads+asc&rows=150&page=&output=json`
+const defaultOptions = '&fl%5B%5D=year&fl%5B%5D=title&fl%5B%5D=mediatype&fl%5B%5D=language&fl%5B%5D=date&fl%5B%5D=downloads&fl%5B%5D=creator&&fl%5B%5D=identifier&sort%5B%5D=year+asc&sort%5B%5D=downloads+asc&rows=150&page=1&output=json'
 
 module.exports = defaultOptions
