56bb8f52e117c53400f4b5bd063317c4147f2e2b
jacobg1 <greenwald.j8@gmail.com>
2024-06-25 12:31:08 -0400

    replace mocha-jsdom with jsdom-global

--- tests/testSearch.js
+++ tests/testSearch.js
@@ -1,121 +1,113 @@
 /* eslint no-unused-expressions: 0 */
-const mocha = require('mocha')
-const { expect } = require('chai')
-const jsdom = require('mocha-jsdom')
+const mocha = require("mocha");
+const { expect } = require("chai");
+const jsdom = require("jsdom-global");
 
-const { describe } = mocha
-const { it } = mocha
-const sinon = require('sinon')
+const { describe } = mocha;
+const { it } = mocha;
+const sinon = require("sinon");
 
-const { archiveSearch } = require('../src/lib/Search')
-const { Search } = require('../src/lib/Search')
+const { archiveSearch } = require("../src/lib/Search");
+const { Search } = require("../src/lib/Search");
 
-describe('Search', () => {
-  describe('constructSearchUrl', () => {
-    it('should be called once', () => {
-      sinon.spy(archiveSearch, 'constructSearchUrl')
-      archiveSearch.constructSearchUrl('creator', 'test')
-      expect(archiveSearch.constructSearchUrl.calledOnce).to.be.true
-    })
-    it('return a string', () => {
-      const searchString = archiveSearch.constructSearchUrl('creator', 'test')
-      expect(searchString).to.be.a('string')
-    })
-    it('string includes base url and function params', () => {
-      const searchString = archiveSearch.constructSearchUrl('creator', 'test')
-      expect(searchString).to.satisfy(string => ['test', 'creator', 'https://archive.org/advancedsearch.php'].every(bit => string.includes(bit)))
-    })
-  })
-  describe('search', () => {
+describe("Search", () => {
+  describe("constructSearchUrl", () => {
+    it("should be called once", () => {
+      sinon.spy(archiveSearch, "constructSearchUrl");
+      archiveSearch.constructSearchUrl("creator", "test");
+      expect(archiveSearch.constructSearchUrl.calledOnce).to.be.true;
+    });
+    it("return a string", () => {
+      const searchString = archiveSearch.constructSearchUrl("creator", "test");
+      expect(searchString).to.be.a("string");
+    });
+    it("string includes base url and function params", () => {
+      const searchString = archiveSearch.constructSearchUrl("creator", "test");
+      expect(searchString).to.satisfy((string) =>
+        ["test", "creator", "https://archive.org/advancedsearch.php"].every((bit) =>
+          string.includes(bit)
+        )
+      );
+    });
+  });
+  describe("search", () => {
     jsdom({
-      url: 'http://localhost',
-    })
+      url: "http://localhost",
+    });
 
-    it('should be called once', () => {
-      sinon.spy(archiveSearch, 'search')
-      archiveSearch.search('test')
-      expect(archiveSearch.search.calledOnce).to.be.true
-    })
-    it('return an array of objects', () => {
-      const search = archiveSearch.search('test')
+    it("should be called once", () => {
+      sinon.spy(archiveSearch, "search");
+      archiveSearch.search("test");
+      expect(archiveSearch.search.calledOnce).to.be.true;
+    });
+    it("return an array of objects", () => {
+      const search = archiveSearch.search("test");
       search.then((result) => {
-        expect(result).to.be.an('array')
-        result.every(i => expect(i).to.be.an('object'))
-      })
-    })
-  })
-  describe('setOptions', () => {
-    sinon.spy(archiveSearch, 'setOptions')
+        expect(result).to.be.an("array");
+        result.every((i) => expect(i).to.be.an("object"));
+      });
+    });
+  });
+  describe("setOptions", () => {
+    sinon.spy(archiveSearch, "setOptions");
     beforeEach(() => {
       const options = {
-        fields: [
-          'year',
-          'title',
-          'mediatype',
-          'language',
-        ],
+        fields: ["year", "title", "mediatype", "language"],
         max: 50,
-      }
-      archiveSearch.setOptions(options)
-    })
-    it('should be called once', () => {
-      expect(archiveSearch.setOptions.calledOnce).to.be.true
-    })
-    it('should take in an argument representing api search options', () => {
-      expect(archiveSearch.setOptions.getCall(0).args[0]).to.not.be.false
-    })
-    it('options argument should be an object', () => {
-      expect(archiveSearch.setOptions.getCall(0).args[0]).to.be.an('object')
-    })
-    it('options.fields should be an array', () => {
-      expect(archiveSearch.setOptions.getCall(0).args[0].fields).to.be.an('array')
-    })
-  })
-  describe('setSortBy', () => {
-    sinon.spy(Search, 'setSortBy')
+      };
+      archiveSearch.setOptions(options);
+    });
+    it("should be called once", () => {
+      expect(archiveSearch.setOptions.calledOnce).to.be.true;
+    });
+    it("should take in an argument representing api search options", () => {
+      expect(archiveSearch.setOptions.getCall(0).args[0]).to.not.be.false;
+    });
+    it("options argument should be an object", () => {
+      expect(archiveSearch.setOptions.getCall(0).args[0]).to.be.an("object");
+    });
+    it("options.fields should be an array", () => {
+      expect(archiveSearch.setOptions.getCall(0).args[0].fields).to.be.an("array");
+    });
+  });
+  describe("setSortBy", () => {
+    sinon.spy(Search, "setSortBy");
     const options = {
-      fields: [
-        'year',
-        'title',
-        'mediatype',
-        'language',
-        'date',
-        'downloads',
-      ],
+      fields: ["year", "title", "mediatype", "language", "date", "downloads"],
       max: 50,
       sortBy: {
-        year: 'asc',
-        downloads: 'asc',
+        year: "asc",
+        downloads: "asc",
       },
-    }
-    Search.setSortBy(options.sortBy)
-    it('should be called once', () => {
-      expect(Search.setSortBy.calledOnce).to.be.true
-    })
-  })
-  describe('metaSearch', () => {
+    };
+    Search.setSortBy(options.sortBy);
+    it("should be called once", () => {
+      expect(Search.setSortBy.calledOnce).to.be.true;
+    });
+  });
+  describe("metaSearch", () => {
     jsdom({
-      url: 'http://localhost',
-    })
-    const testId = 'gd1967-xx-xx.sbd.studio.81259.flac16'
+      url: "http://localhost",
+    });
+    const testId = "gd1967-xx-xx.sbd.studio.81259.flac16";
 
-    it('should be called once', () => {
-      sinon.spy(archiveSearch, 'metaSearch')
-      archiveSearch.metaSearch(testId)
-      expect(archiveSearch.metaSearch.calledOnce).to.be.true
-    })
-    it('return an object', () => {
+    it("should be called once", () => {
+      sinon.spy(archiveSearch, "metaSearch");
+      archiveSearch.metaSearch(testId);
+      expect(archiveSearch.metaSearch.calledOnce).to.be.true;
+    });
+    it("return an object", () => {
       archiveSearch.metaSearch(testId).then((result) => {
-        expect(result).to.be.an('object')
-      })
-    })
-    it('object should have keys: metadata, reviews, files', () => {
-      const objectProperties = ['metadata', 'reviews', 'files']
+        expect(result).to.be.an("object");
+      });
+    });
+    it("object should have keys: metadata, reviews, files", () => {
+      const objectProperties = ["metadata", "reviews", "files"];
       archiveSearch.metaSearch(testId).then((result) => {
         Object.keys(result).forEach((item) => {
-          expect(objectProperties).to.include(item)
-        })
-      })
-    })
-  })
-})
+          expect(objectProperties).to.include(item);
+        });
+      });
+    });
+  });
+});
