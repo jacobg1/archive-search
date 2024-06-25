3ea4eb35bcb53847379ed11dbfcb9d46bd385cd0
jacobg1 <greenwald.j8@gmail.com>
2024-06-25 12:22:21 -0400

    update dependencies

--- webpack.config.browser.js
+++ webpack.config.browser.js
@@ -1,34 +1,29 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-	entry: './src/lib/Search.js',
-	output: {
-	path: path.resolve(__dirname, './src/dist'),
-	filename: 'search-browser.min.js',
-    library: 'archiveSearch',
-    libraryTarget: 'umd',
+  entry: "./src/lib/Search.js",
+  output: {
+    path: path.resolve(__dirname, "./src/dist"),
+    filename: "search-browser.min.js",
+    library: "archiveSearch",
+    libraryTarget: "umd",
     umdNamedDefine: true,
-    globalObject: "typeof self !== 'undefined' ? self : this"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				include: path.resolve(__dirname, 'src'),
-				exclude: /(node_modules|bower_components|build)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			}
-		]
-	},
-	target: 'web',
-	node: {
-		console: true,
-		fs: 'empty',
-		net: 'empty',
-		tls: 'empty'
-	}
+    globalObject: "typeof self !== 'undefined' ? self : this",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  target: "web",
+  node: false,
 };
