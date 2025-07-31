0211139adb672e0a66163a5d08f1427e67b0c364
jacobg1 <greenwald.j8@gmail.com>
2025-07-31 09:41:19 -0400

    update test file

--- test.js
+++ test.js
@@ -1,11 +1,17 @@
 const { archiveSearch } = require("./src/index");
-
+const options = {
+  searchBy: "creator",
+  fields: ["date", "description", "format", "mediatype", "source", "title"],
+  max: 50,
+  sortBy: {
+    downloads: "desc",
+  },
+};
 (async () => {
-  const testing = await archiveSearch.search("Grateful+Dead+AND+year%3A1994", {
-    searchBy: "creator",
-    fields: ["description", "format", "mediatype", "source", "title"],
-    max: 500,
-    sortBy: { date: "desc", downloads: "desc" },
-  });
-  console.log("testing", testing);
+  const testing = await archiveSearch.search("Grateful+Dead+AND+year%3A1994", options);
+  console.log(JSON.stringify(testing, null, 4));
+  const metaTestSearch = await archiveSearch.metaSearch(
+    "gd1977-12-31.151332.sbd.bear.gems.V3.flac1648"
+  );
+  console.log(JSON.stringify(metaTestSearch, null, 4));
 })();
